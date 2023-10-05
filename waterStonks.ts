67c9184eab211f75ae44192d05e3ed1f567bfb2b
Ethan Crann <ecrann@protonmail.com>
2023-10-04 23:22:59 -0230

    Max is a year, will have to come up with another method

--- waterStonks.ts
+++ waterStonks.ts
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 dotenv.config();
 const usernames = ["mynameisgump"];
+const date = new Date();
 
 const getContributions = async (token: string, username: string) => {
   const headers = {
@@ -10,7 +11,7 @@ const getContributions = async (token: string, username: string) => {
     query: `query {
             user(login: "${username}") {
               name
-              contributionsCollection {
+              contributionsCollection(from: "2008-09-28T23:05:23Z", to: "2023-10-02T23:05:23Z"){
                 contributionCalendar {
                   colors
                   totalContributions
@@ -36,8 +37,8 @@ const getContributions = async (token: string, username: string) => {
   const data = await response.json();
   return data;
 };
-const data = await getContributions(process.env.GITHUB_TOKEN!, "mynameisgump");
-console.log(data);
-
+const data = await getContributions(process.env.GITHUB_TOKEN!, "nint8835");
+const path = "./nint8835.txt";
+await Bun.write(path, JSON.stringify(data));
 console.log("Hello World!");
 export default getContributions;
