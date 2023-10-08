a8e8266d98fadd50a00e16a5f90b82c1e98a3de6
Ethan Crann <ecrann@protonmail.com>
2023-10-08 14:20:06 -0230

    Db initialization has been added to the main function

--- waterStonks.ts
+++ waterStonks.ts
@@ -1,11 +1,32 @@
+import { Database } from "bun:sqlite";
 import dotenv from "dotenv";
 dotenv.config();
 const usernames = ["mynameisgump"];
 const date = new Date();
 // Beginning "2008-01-01T00:00:00Z"
 // End"2009-12-31T23:59:59Z"
+// User Table:
 
-import { Database } from "bun:sqlite";
+//     user_id (Primary Key): Unique identifier for each GitHub user.
+//     github_username: The GitHub username of the user.
+
+// Commit Activity Table:
+
+//     activity_id (Primary Key): Unique identifier for each daily commit activity record.
+//     user_id (Foreign Key): References the user_id from the User Table.
+//     date: Date of the commit activity.
+//     total_commits: Total number of commits made by the user on that day.
+
+const initGumpDb = async () => {
+  const db = new Database("gumpdb.sqlite");
+  await db.query(
+    "CREATE TABLE IF NOT EXISTS user (user_id INTEGER PRIMARY KEY, github_username TEXT NOT NULL);"
+  );
+  await db.query(
+    "CREATE TABLE IF NOT EXISTS commit_activity (activity_id INTEGER PRIMARY KEY, user_id INTEGER NOT NULL, date TEXT NOT NULL, total_commits INTEGER NOT NULL, FOREIGN KEY(user_id) REFERENCES user(user_id));"
+  );
+  return db;
+};
 
 const getDateJoinedGithub = async (token: string, username: string) => {
   const headers = {
@@ -85,14 +106,19 @@ const getContributions = async (token: string, username: string) => {
 };
 
 const main = async () => {
+  const db = await initGumpDb();
+
   // const data = await getContributions(process.env.GITHUB_TOKEN!, "nint8835");
-  console.log("Test");
+  // console.log("Test");
   // const data = await getYearsContributed(process.env.GITHUB_TOKEN!, "nint8835");
   // console.log(data);
 
   // const path = "./nint8835.txt";
   // await Bun.write(path, JSON.stringify(data));
   // console.log("Hello World!");
+
+  // const query = db.query("select 'Hello world' as message;");
+  // query.get(); // => { message: "Hello world" }
 };
 
 main();
