f9fdf9e92375e851f2e3376f41e148cc61856ea2
Ethan Crann <ecrann@protonmail.com>
2023-10-09 00:40:48 -0230

    simplified process script

--- processStonks.ts
+++ processStonks.ts
@@ -1,4 +1,8 @@
 import { Database } from "bun:sqlite";
+type UserInfo = {
+  user_id: number;
+  github_username: string;
+};
 
 type CommitActivity = {
   activity_id: number;
@@ -6,24 +10,36 @@ type CommitActivity = {
   date: string;
   total_commits: number;
 };
-//  `SELECT * FROM commit_activity WHERE user_id = 2 AND date BETWEEN "2008-01-01T00:00:00Z" AND "2009-12-31T23:59:59Z"`
 const db = new Database("gumpdb.sqlite");
-const nintQuery = db.query<CommitActivity, null>(
-  `SELECT * FROM commit_activity WHERE user_id = 2`
-);
-const gumpQuery = db.query<CommitActivity, null>(
-  `SELECT * FROM commit_activity WHERE user_id = 1`
-);
+const allUsersQuery = db.query<UserInfo, null>("SELECT * FROM user");
+const users = allUsersQuery.all(null);
 
-const nintData = nintQuery.all(null);
-const gumpData = gumpQuery.all(null);
-
-const data = [nintData, gumpData];
-
-for (const user of data) {
-  const totalCommits = user.reduce((acc, curr) => acc + curr.total_commits, 0);
+for (let user of users) {
+  console.log(user.github_username);
+  const commitsQuery = db.query<CommitActivity, number>(
+    `SELECT * FROM commit_activity WHERE user_id = $user`
+  );
+  const commits = commitsQuery.all(user.user_id);
+  const totalCommits = commits.reduce(
+    (acc, curr) => acc + curr.total_commits,
+    0
+  );
   console.log("User Commits:", totalCommits);
 }
 
-// console.log(nintData);
-console.log();
+//  `SELECT * FROM commit_activity WHERE user_id = 2 AND date BETWEEN "2008-01-01T00:00:00Z" AND "2009-12-31T23:59:59Z"`
+// const db = new Database("gumpdb.sqlite");
+// const nintQuery = db.query<CommitActivity, null>(
+//   `SELECT * FROM commit_activity WHERE user_id = 2`
+// );
+// const gumpQuery = db.query<CommitActivity, null>(
+//   `SELECT * FROM commit_activity WHERE user_id = 1`
+// );
+
+// const nintData = nintQuery.all(null);
+// const gumpData = gumpQuery.all(null);
+
+// const data = [nintData, gumpData];
+
+// // console.log(nintData);
+// console.log();
