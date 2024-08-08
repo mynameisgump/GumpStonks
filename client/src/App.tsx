17305fba86a7f385a5242a325721dc8aed31e4f3
Ethan Crann <ecrann@protonmail.com>
2024-08-08 11:29:52 -0230

    oh boy here we go

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,44 +1,206 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { ResponsiveLineCanvas } from '@nivo/line'
-import KeenanData from './assets/Keenan-Nicholson.json'
+import { format } from "d3-format";
+import { timeFormat } from "d3-time-format";
+import {
+  elderRay,
+  ema,
+  discontinuousTimeScaleProviderBuilder,
+  Chart,
+  ChartCanvas,
+  CurrentCoordinate,
+  BarSeries,
+  CandlestickSeries,
+  ElderRaySeries,
+  LineSeries,
+  MovingAverageTooltip,
+  OHLCTooltip,
+  SingleValueTooltip,
+  lastVisibleItemBasedZoomAnchor,
+  XAxis,
+  YAxis,
+  CrossHairCursor,
+  EdgeIndicator,
+  MouseCoordinateX,
+  MouseCoordinateY,
+  ZoomButtons,
 
+} from "react-financial-charts";
+import { initialData } from "./assets/mynameisgump.ts";
 
-function App() {
-  console.log(KeenanData)
+const App = () => {
+  const ScaleProvider = discontinuousTimeScaleProviderBuilder().inputDateAccessor(
+    (d) => new Date(d.date)
+  );
+  const height = 700;
+  const width = 900;
+  const margin = { left: 0, right: 48, top: 0, bottom: 24 };
+
+  const ema12 = ema()
+    .id(1)
+    .options({ windowSize: 12 })
+    .merge((d, c) => {
+      d.ema12 = c;
+    })
+    .accessor((d) => d.ema12);
+
+  const ema26 = ema()
+    .id(2)
+    .options({ windowSize: 26 })
+    .merge((d, c) => {
+      d.ema26 = c;
+    })
+    .accessor((d) => d.ema26);
+
+  const elder = elderRay();
+
+  const calculatedData = elder(ema26(ema12(initialData)));
+  const { data, xScale, xAccessor, displayXAccessor } = ScaleProvider(
+    initialData
+  );
+  const pricesDisplayFormat = format(".2f");
+  const max = xAccessor(data[data.length - 1]);
+  const min = xAccessor(data[Math.max(0, data.length - 100)]);
+  const xExtents = [min, max + 5];
+
+  const gridHeight = height - margin.top - margin.bottom;
+
+  const elderRayHeight = 100;
+  const elderRayOrigin = (_, h) => [0, h - elderRayHeight];
+  const barChartHeight = gridHeight / 4;
+  const barChartOrigin = (_, h) => [0, h - barChartHeight - elderRayHeight];
+  const chartHeight = gridHeight - elderRayHeight;
+  const yExtents = (data) => {
+    return [data.high, data.low];
+  };
+  const dateTimeFormat = "%d %b";
+  const timeDisplayFormat = timeFormat(dateTimeFormat);
+
+  const barChartExtents = (data) => {
+    return data.volume;
+  };
+
+  const candleChartExtents = (data) => {
+    return [data.high, data.low];
+  };
+
+  const yEdgeIndicator = (data) => {
+    return data.close;
+  };
+
+  const volumeColor = (data) => {
+    return data.close > data.open
+      ? "rgba(38, 166, 154, 0.3)"
+      : "rgba(239, 83, 80, 0.3)";
+  };
+
+  const volumeSeries = (data) => {
+    return data.volume;
+  };
+
+  const openCloseColor = (data) => {
+    return data.close > data.open ? "#26a69a" : "#ef5350";
+  };
+
   return (
-    <>
-      <div style={{height: "1000px", width: "1000px"}}>
-        <ResponsiveLineCanvas
-          data={[KeenanData]}
-          yScale={{ type: 'linear' }}
-          // enableGridX={false}
-          margin={{ top: 50, right: 160, bottom: 50, left: 60 }}
-          gridXValues={["2021-01-01T00:00:00.000Z","2022-01-01T00:00:00.000Z","2023-01-01T00:00:00.000Z","2024-01-01T00:00:00.000Z"]}
-          axisBottom={{
-            tickValues: [
-                0,
-                20,
-                40,
-                60,
-                80,
-                100,
-                120
-            ],
-            tickSize: 5,
-            tickPadding: 5,
-            tickRotation: 0,
-            format: '.2f',
-            legend: 'price',
-            legendOffset: 36,
-            legendPosition: 'middle'
-        }}
+    <ChartCanvas
+      height={height}
+      ratio={3}
+      width={width}
+      margin={margin}
+      data={data}
+      displayXAccessor={displayXAccessor}
+      seriesName="Data"
+      xScale={xScale}
+      xAccessor={xAccessor}
+      xExtents={xExtents}
+      zoomAnchor={lastVisibleItemBasedZoomAnchor}
+    >
+      <Chart
+        id={2}
+        height={barChartHeight}
+        origin={barChartOrigin}
+        yExtents={barChartExtents}
+      >
+        <BarSeries fillStyle={volumeColor} yAccessor={volumeSeries} />
+      </Chart>
+      <Chart id={3} height={chartHeight} yExtents={candleChartExtents}>
+        <XAxis showGridLines showTickLabel={false} />
+        <YAxis showGridLines tickFormat={pricesDisplayFormat} />
+        <CandlestickSeries />
+        <LineSeries yAccessor={ema26.accessor()} strokeStyle={ema26.stroke()} />
+        <CurrentCoordinate
+          yAccessor={ema26.accessor()}
+          fillStyle={ema26.stroke()}
         />
-      </div>
-      
-    </>
-  )
-}
+        <LineSeries yAccessor={ema12.accessor()} strokeStyle={ema12.stroke()} />
+        <CurrentCoordinate
+          yAccessor={ema12.accessor()}
+          fillStyle={ema12.stroke()}
+        />
+        <MouseCoordinateY
+          rectWidth={margin.right}
+          displayFormat={pricesDisplayFormat}
+        />
+        <EdgeIndicator
+          itemType="last"
+          rectWidth={margin.right}
+          fill={openCloseColor}
+          lineStroke={openCloseColor}
+          displayFormat={pricesDisplayFormat}
+          yAccessor={yEdgeIndicator}
+        />
+        <MovingAverageTooltip
+          origin={[8, 24]}
+          options={[
+            {
+              yAccessor: ema26.accessor(),
+              type: "EMA",
+              stroke: ema26.stroke(),
+              windowSize: ema26.options().windowSize
+            },
+            {
+              yAccessor: ema12.accessor(),
+              type: "EMA",
+              stroke: ema12.stroke(),
+              windowSize: ema12.options().windowSize
+            }
+          ]}
+        />
 
-export default App
+        <ZoomButtons />
+        <OHLCTooltip origin={[8, 16]} />
+      </Chart>
+      <Chart
+        id={4}
+        height={elderRayHeight}
+        yExtents={[0, elder.accessor()]}
+        origin={elderRayOrigin}
+        padding={{ top: 8, bottom: 8 }}
+      >
+        <XAxis showGridLines gridLinesStrokeStyle="#e0e3eb" />
+        <YAxis ticks={4} tickFormat={pricesDisplayFormat} />
+
+        <MouseCoordinateX displayFormat={timeDisplayFormat} />
+        <MouseCoordinateY
+          rectWidth={margin.right}
+          displayFormat={pricesDisplayFormat}
+        />
+
+        <ElderRaySeries yAccessor={elder.accessor()} />
+
+        <SingleValueTooltip
+          yAccessor={elder.accessor()}
+          yLabel="Elder Ray"
+          yDisplayFormat={(d) =>
+            `${pricesDisplayFormat(d.bullPower)}, ${pricesDisplayFormat(
+              d.bearPower
+            )}`
+          }
+          origin={[8, 16]}
+        />
+      </Chart>
+      <CrossHairCursor />
+    </ChartCanvas>
+  );
+};
+
+export default App;
